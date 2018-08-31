6c52cda5520b52be25d9d2bdd546dc91cc82fa10
Nicolas Franchi <nicolas.franchi@stu.jefferson.kyschools.us>
2018-08-31 09:11:46 -0400

    first commit

---
+++ 8 ball game.py
@@ -0,0 +1,18 @@
+
+import time
+import sys
+ball = input("I can answer any one of your questions, so ask me ")
+if ball == "no":
+    print("oh ok")
+    sys.exit
+if ball == "":
+    sys.exit
+import random
+count = random.randint(1, 4)
+if ball == '':
+    if count = 1
+    print ("poor")
+    if count = 2
+    print("yes")
+    if count = 3
+    print("no")
